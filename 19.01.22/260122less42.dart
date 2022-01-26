ac221fe9be70cab7c9417f9a1b3e8c1c89bf030b
fortuntim <timurov@yandex.ru>
2022-01-26 18:46:22 +0300

    260122.lesson42.dart

---
+++ 19.01.22/260122less42.dart
@@ -0,0 +1,8 @@
+void main() {
+
+  Character person = Character('Анифа', 'Мужской', 18, 88);
+  person.info();
+
+  Character maga = Character.magomed('Мухаммад', 'Мужской', 19, 90);
+  maga.info();
+}
